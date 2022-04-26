e364d6fbd0b9ca9ea744f1df457031c789c7c1e6
unknown <taminojudit@gmail.com>
2022-04-26 09:07:03 +0300

    Organize Layers Model

---
+++ Judith-Tech-Weather.Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Judith_Tech_Weather.Model
+{
+    public class Class1
+    {
+    }
+}
